3cca721db1e3df6ef25d3e60c6a12c526252b0c4
rkilic1-spec <r.kilic1@gmail.com>
2026-01-08 11:46:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const http = require("http");
 const PORT = process.env.PORT || 10000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("VAN ÖZEL DİYALİZ MERKEZİ - SUNUCU CALISIYOR");
+  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+  res.end("VAN ÖZEL DİYALİZ MERKEZİ - SUNUCU ÇALIŞIYOR");
 });
 
 server.listen(PORT, "0.0.0.0", () => {
