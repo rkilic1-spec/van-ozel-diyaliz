ed624aaaf442cd9a046ed37a5cd95be87aa30240
rkilic1-spec <r.kilic1@gmail.com>
2026-01-08 17:57:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,26 @@ app.use(
     saveUninitialized: true,
   })
 );
+function requireLogin(req, res, next) {
+  if (!req.session.user) {
+    return res.redirect("/");
+  }
+  next();
+}
+
+function requireAdmin(req, res, next) {
+  if (!req.session.user || req.session.user.role !== "admin") {
+    return res.redirect("/");
+  }
+  next();
+}
+
+function requireHemsire(req, res, next) {
+  if (!req.session.user || req.session.user.role !== "hemsire") {
+    return res.redirect("/");
+  }
+  next();
+}
 
 // ---- KULLANICI ----
 const USER = {
