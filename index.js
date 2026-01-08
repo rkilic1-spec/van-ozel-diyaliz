03e8a332038fd1a160423b97ee9d691482644de6
rkilic1-spec <r.kilic1@gmail.com>
2026-01-08 18:26:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const path = require("path");
 const express = require("express");
 const session = require("express-session");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 10000;
