adadda01c91e1bd6dd733b552e47a90bfcf73ba3
rkilic1-spec <r.kilic1@gmail.com>
2026-01-11 11:46:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 10000;
 /* ================== MIDDLEWARE ================== */
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/data", express.static(path.join(__dirname, "data")));
 
 app.use(
   session({
